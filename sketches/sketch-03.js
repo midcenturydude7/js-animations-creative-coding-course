751859bd3233d839038875f4f903d00b1d550926
midCenturyDude7 <mgriffes@gmail.com>
2025-01-25 21:06:26 -0800

    Working with animations

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -1,5 +1,5 @@
 import canvasSketch from "canvas-sketch";
-import { random } from "canvas-sketch-util";
+import { random, math } from "canvas-sketch-util";
 
 const settings = {
   dimensions: [1080, 1080],
@@ -27,6 +27,24 @@ const sketch = ({ width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) {
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 200) continue;
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1);
+
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     agents.forEach((agent) => {
       agent.update();
       agent.draw(context);
@@ -41,6 +59,12 @@ class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+  }
+
+  getDistance(v) {
+    const dx = this.x - v.x;
+    const dy = this.y - v.y;
+    return Math.sqrt(dx * dx + dy * dy);
   }
 }
 
