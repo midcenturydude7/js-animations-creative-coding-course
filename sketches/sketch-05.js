48a12d84999c102815e102db480e94e2e23e54fc
midCenturyDude7 <mgriffes@gmail.com>
2025-01-31 22:52:44 -0800

    Playing with symbols

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -99,9 +99,9 @@ const getGlyph = (v) => {
   if (v < 50) return "";
   if (v < 100) return ".";
   if (v < 150) return "-";
-  if (v < 200) return "+";
+  if (v < 200) return ";";
 
-  const glyphs = "_= /".split("");
+  const glyphs = "_= *".split("");
 
   return random.pick(glyphs);
 };
